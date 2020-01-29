7438a1f4ba9821fab6740969cd8e38ec3eec81cd
IrvinS <isanchez@applaudostudios.com>
2020-01-29 17:11:19 -0600

    creacion de store redux

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import createSagaMiddleware from 'redux-saga';
+
+import history from './history';
+import reducer from './reducers';
+import rootSaga from './sagas';
+
+const initialState = {};
+const sagaMiddleware = createSagaMiddleware();
+const routersMiddleware = routerMiddleware(history);
+let middlewares = applyMiddleware(sagaMiddleware, routersMiddleware);
+
+const store = createStore(reducer, initialState, composeWithDevTools(middlewares));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
